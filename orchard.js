b368a19bae26ff8a45e2c6829bfc3ad78caa3d13
Mike Whaley <michael.d.whaley@gmail.com>
2022-11-19 09:09:14 -0500

    finished and formatted

--- orchard.js
+++ orchard.js
@@ -27,15 +27,13 @@
     in cents. 
 */
 
-const fujiAcres = [2, 3, 3, 2, 2, 2, 1]
-const galaAcres = [5, 2, 4, 3, 6, 2, 4]
-const pinkAcres = [1, 5, 4, 2, 1, 5, 4]
+const fujiAcres = [2, 3, 3, 2, 2, 2, 1];
+const galaAcres = [5, 2, 4, 3, 6, 2, 4];
+const pinkAcres = [1, 5, 4, 2, 1, 5, 4];
 
-const fujiPrice = .89 
-const galaPrice = .64
-const pinkPrice = .55
-
-
+const fujiPrice = 0.89;
+const galaPrice = 0.64;
+const pinkPrice = 0.55;
 
 // PROBLEM 1
 
@@ -51,18 +49,15 @@ const pinkPrice = .55
 
 // CODE HERE
 //Declare the totalAcres variable
-let totalAcres = 0
-// create the for loop to iterate through the days of the week 
+let totalAcres = 0;
+// create the for loop to iterate through the days of the week
 // using the fujiAcres array length
 for (let i = 0; i < fujiAcres.length; i++) {
-    //add each orchards acres per day to the total acres variable
-    totalAcres += fujiAcres[i] + galaAcres[i] + pinkAcres[i]
+  //add each orchards acres per day to the total acres variable
+  totalAcres += fujiAcres[i] + galaAcres[i] + pinkAcres[i];
 }
 //log the totalAcres should be 63
-console.log(totalAcres)
-
-
-
+console.log(totalAcres);
 
 // PROBLEM 2
 
@@ -77,15 +72,12 @@ console.log(totalAcres)
 */
 
 // CODE HERE
-//created the averageDailyAcres variable by dividing 
-// the totalAcres by the fuji array length which is 
+//created the averageDailyAcres variable by dividing
+// the totalAcres by the fuji array length which is
 // 7(days in a week) and the average ends up 63/7 = 9.
-let averageDailyAcres = totalAcres / fujiAcres.length
+let averageDailyAcres = totalAcres / fujiAcres.length;
 //log the average which ends up 9
-console.log(averageDailyAcres)
-
-
-
+console.log(averageDailyAcres);
 
 // PROBLEM 3
 
@@ -116,21 +108,19 @@ console.log(averageDailyAcres)
 
 */
 
-let acresLeft = 174 
-let days = 0
+let acresLeft = 174;
+let days = 0;
 
 // CODE HERE
 //created a while loop that will run as long as there are acres left
 while (acresLeft > 0) {
-    //adds a day to the days variable as average daily acres are picked
-    days += 1
-    //subtracts the days harvest from acresLeft
-    acresLeft -= averageDailyAcres
+  //adds a day to the days variable as average daily acres are picked
+  days += 1;
+  //subtracts the days harvest from acresLeft
+  acresLeft -= averageDailyAcres;
 }
 //log the number of days it took to pick 174 acres
-console.log(days)
-
-
+console.log(days);
 
 // PROBLEM 4
 
@@ -158,28 +148,26 @@ console.log(days)
 
 // CODE HERE
 // Create a tonsPerAcre variable in case it ever changes
-let tonsPerAcre = 6.5
+let tonsPerAcre = 6.5;
 // create the three arrays empty
-let fujiTons = []
-let galaTons = []
-let pinkTons = []
-// loop the fujiAcres array and multiply each day acres 
+let fujiTons = [];
+let galaTons = [];
+let pinkTons = [];
+// loop the fujiAcres array and multiply each day acres
 // by tonsPerAcre and push that value to the fujiTons array
 for (let i = 0; i < fujiAcres.length; i++) {
-    fujiTons.push(fujiAcres[i] * tonsPerAcre)
+  fujiTons.push(fujiAcres[i] * tonsPerAcre);
 }
-// loop the galaAcres array and multiply each day acres 
+// loop the galaAcres array and multiply each day acres
 // by tonsPerAcre and push that value to the galaTons array
 for (let i = 0; i < galaAcres.length; i++) {
-    galaTons.push(galaAcres[i] * tonsPerAcre)
+  galaTons.push(galaAcres[i] * tonsPerAcre);
 }
-// loop the pinkAcres array and multiply each day acres 
+// loop the pinkAcres array and multiply each day acres
 // by tonsPerAcre and push that value to the pinkTons array
 for (let i = 0; i < pinkAcres.length; i++) {
-    pinkTons.push(pinkAcres[i] * tonsPerAcre)
+  pinkTons.push(pinkAcres[i] * tonsPerAcre);
 }
-
-
 
 // PROBLEM 5
 
@@ -197,44 +185,42 @@ for (let i = 0; i < pinkAcres.length; i++) {
     Hint: there are 2000 pounds in a ton.
 */
 
-// CODE HERE 
+// CODE HERE
 // declare the variables for the varieties
-let fujiPounds = 0
-let galaPounds = 0
-let pinkPounds = 0
+let fujiPounds = 0;
+let galaPounds = 0;
+let pinkPounds = 0;
 // created a poundsPerTon constant because pounds per ton
 // should never change, however, what if we wanted to calculate
-// by metric ton later and had 100 varieties? We could change 
+// by metric ton later and had 100 varieties? We could change
 // this constant in the code here once
-const poundsPerTon = 2000
+const poundsPerTon = 2000;
 // iterate through the fujiTons loop adding each
-// value to sum up total tons and then multiplying 
+// value to sum up total tons and then multiplying
 // by poundsPerTon to get the pounds value.
 for (let i = 0; i < fujiTons.length; i++) {
-    fujiPounds += fujiTons[i] * poundsPerTon
+  fujiPounds += fujiTons[i] * poundsPerTon;
 }
 // log the pounds
-console.log(fujiPounds)
+console.log(fujiPounds);
 
 // iterate through the galaTons loop adding each
-// value to sum up total tons and then multiplying 
+// value to sum up total tons and then multiplying
 // by poundsPerTon to get the pounds value.
 for (let i = 0; i < galaTons.length; i++) {
-    galaPounds += galaTons[i] * poundsPerTon
+  galaPounds += galaTons[i] * poundsPerTon;
 }
 // log the pounds
-console.log(galaPounds)
+console.log(galaPounds);
 
 // iterate through the pinkTons loop adding each
-// value to sum up total tons and then multiplying 
+// value to sum up total tons and then multiplying
 // by poundsPerTon to get the pounds value.
 for (let i = 0; i < pinkTons.length; i++) {
-    pinkPounds += pinkTons[i] * poundsPerTon
+  pinkPounds += pinkTons[i] * poundsPerTon;
 }
 // log the pounds
-console.log(pinkPounds)
-
-
+console.log(pinkPounds);
 
 // PROBLEM 6
 
@@ -253,15 +239,15 @@ console.log(pinkPounds)
 */
 
 // CODE HERE
-
-// let fujiProfit =
-// let galaProfit =
-// let pinkProfit =
-
-
-
-
-
+// creating the profit variables by multiplying
+// the respective pounds variables by the price
+let fujiProfit = fujiPounds * fujiPrice;
+let galaProfit = galaPounds * galaPrice;
+let pinkProfit = pinkPounds * pinkPrice;
+// log the profit variables
+console.log(fujiProfit);
+console.log(galaProfit);
+console.log(pinkProfit);
 
 // PROBLEM 7
 
@@ -274,3 +260,19 @@ console.log(pinkPounds)
 */
 
 // CODE HERE
+// create the totalProfit variable by adding all profit
+let totalProfit = fujiProfit + galaProfit + pinkProfit;
+// log the totalProfit
+console.log(totalProfit);
+
+// Convert to dollars
+// CODE HERE
+// create a conversion from pennies to dollars and make it
+// more readable
+const penniesPerDollar = 100;
+// create
+// log all penny values to dollar
+console.log(`Profit for Fuji: $${fujiProfit / penniesPerDollar}`);
+console.log(`Profit for Gala: $${galaProfit / penniesPerDollar}`);
+console.log(`Profit for Pink: $${pinkProfit / penniesPerDollar}`);
+console.log(`Total profit: $${totalProfit / penniesPerDollar}`);
